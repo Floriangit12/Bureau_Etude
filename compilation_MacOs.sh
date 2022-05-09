4b6c7e53ea68b5117622172be7b8a2d36bb435cd
Floriangit12 <76617468+Floriangit12@users.noreply.github.com>
2022-05-09 17:27:13 +0200

    Add files via upload

--- compilation_MacOs.sh
+++ compilation_MacOs.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 java --version
-mkdir 
 git clone https://github.com/Floriangit12/Bureau_Etude
-mkdir Compilation;
-root = pwd
-javac -d Compilation --module-path ${root}/javafx-sdk-18.0.1/lib --add-modules javafx.controls,javafx.fxml Sphere3D.java;
-cd Compilation/;
-java -Djavafx.platform=eglfb -Dprism.verbose=true -Dcom.sun.javafx.experimental.embedded.3d=true  -Dprism.glDepthSize=24 -Dprism.forceGPU=true  --module-path ${root}/javafx-sdk-18.0.1/lib --add-modules javafx.controls,javafx.fxml Sphere3D
+cd Bureau_Etude
+wget https://download2.gluonhq.com/openjfx/18.0.1/openjfx-18.0.1_osx-x64_bin-sdk.zip
+unzip openjfx-18.0.1_osx-x64_bin-sdk.zip
+rm openjfx-18.0.1_osx-x64_bin-sdk.zip
+mv javafx-sdk-18.0.1 javafx-sdk_MacOs-18.0.1
+current_work="$(pwd)"
+JAVAFX_Dir="$current_work/JAVAFX/javafx-sdk_MacOs-18.0.1/lib"
+javac -d Compilation --module-path "$JAVAFX_Dir" --add-modules javafx.controls,javafx.fxml Sphere3D.java
+cd Compilation;
+java -Djavafx.platform=eglfb -Dprism.verbose=true -Dcom.sun.javafx.experimental.embedded.3d=true  -Dprism.glDepthSize=24 -Dprism.forceGPU=true  --module-path "$JAVAFX_Dir" --add-modules javafx.controls,javafx.fxml Sphere3D
